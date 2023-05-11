3753a2348ace6d74d032b671963f454972fee446
Jullian <83075638+JullianWasTaken@users.noreply.github.com>
2023-05-11 13:10:47 -0400

    Initial commit

---
+++ alert.js
@@ -0,0 +1,9 @@
+alert("Anthony is sussy");
+
+function sus() {
+    if (document.getElementById("anthonyChange").src == "https://www.channelstv.com/wp-content/uploads/2015/03/Mike-Omeri.png"){
+        document.getElementById("anthonyChange").src = "https://www.channelstv.com/wp-content/uploads/2015/03/Mike-Omeri.png";
+    } else {
+        document.getElementById("anthonyChange").src = "https://image.isu.pub/130724141447-8b53216cc93ffd9390cde14ed14ff2fb/jpg/page_1.jpg";
+    }
+}
