c23ef7d04c9075e6a4971047f2ade34c8eabeb63
Jullian <83075638+JullianWasTaken@users.noreply.github.com>
2023-05-17 13:20:09 -0400

    Updated JS script
    Included the produce arrays

--- mainScript.js
+++ mainScript.js
@@ -1,6 +1,6 @@
 
 
-/*
+/* Anthony Appa Ross
 function sus() {
     if (document.getElementById("anthonyChange").src == "https://www.channelstv.com/wp-content/uploads/2015/03/Mike-Omeri.png"){
         document.getElementById("anthonyChange").src = "https://www.channelstv.com/wp-content/uploads/2015/03/Mike-Omeri.png";
@@ -10,6 +10,35 @@ function sus() {
         document.getElementById("anthonyChange").src = "https://media.licdn.com/dms/image/C5603AQEpLiC4sv6GiQ/profile-displayphoto-shrink_800_800/0/1654544619047?e=2147483647&v=beta&t=MGXoSeiYHPnMMHqK2_mqWIpqSek2ge2NfQaP5_AZ0Fw";
     } 
 }*/
+
+
+let pluName = [ //0=banana, 1=apple, 2=herbs
+["banana", "organic banana"],
+["gala apple", "honeycrisp apple", "granny smith apple", "macintosh apple"],
+["regular parsley", "curley parsley", "coriander/cilantro", "mint", "dill"]
+];
+
+let pluNums = [ //0=banana, 1=apple, 2=herbs
+["4011", "94011"],
+["4135", "3283", "4139", "4152"],
+["4901", "4899", "4889", "4896", "4891"]
+];
+
+let pluImages = [ //Put images of vegetables later lol /0=banana, 1=apple, 2=herbs
+
+
+];
+
+//RNG Select
+
+
+
+function selectPlu() {
+  let selectedCategory = Math.floor(Math.random()*(pluName.length-1)); //Selects type of produce (banana, apple, herb)
+  let selectedSubCategory = 3; 
+}
+
+
 
 function checkPlu() {
   let givenPLU = document.forms["pluTest"]["plu"].value
